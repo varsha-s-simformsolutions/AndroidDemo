11a8678a246dd87a79e624df865f9fa01b94cc1b
varsha <varsha.s@simformsolutions.com>
2021-01-11 17:16:28 +0530

    TE4-47 Login Form Added

---
+++ app/src/main/java/com/example/first/KotlinActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.first
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KotlinActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin2)
+    }
+}
